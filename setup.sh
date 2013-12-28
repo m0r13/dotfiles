b6dd9879c99a71798b509557bba0244a677deb56
Moritz Hilscher <moritz.hilscher@gmail.com>
2013-12-28 21:54:24 +0100

    Changed '.files' to 'files'.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.bash_exports ~/.bash_exports
-ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
-ln -s ~/dotfiles/.bash_prompt ~/.bash_prompt
+ln -s dotfiles/bashrc ../.bashrc
+ln -s dotfiles/bash_profile ../.bash_profile
+ln -s dotfiles/bash_exports ../.bash_exports
+ln -s dotfiles/bash_aliases ../.bash_aliases
+ln -s dotfiles/bash_prompt ../.bash_prompt
 
-ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s dotfiles/zshrc ../.zshrc
 
-ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s dotfiles/vimrc ../.vimrc
