5b066e9cbbb457e11d5b68e044f3015c839dba5e
m0r13 <moritz.hilscher@gmail.com>
2014-12-02 11:26:03 +0100

    Removed old comments

--- bin/hpi-mount-smb.sh
+++ bin/hpi-mount-smb.sh
@@ -15,5 +15,3 @@ else
     mount -t cifs //FS23/lehrveranstaltungen /media/hpi/lehrveranstaltungen -o username=moritz.hilscher,workgroup=hpi,ip=127.0.0.1,port=7445,uid=moritz,gid=moritz
 fi
 
-#mount -t cifs //FS23/moritz.hilscher$ /media/hpi/home -o credentials=/home/moritz/.local/etc/hpi_sambacreds,workgroup=hpi,uid=moritz,gid=moritz
-#mount -t cifs //FS23/lehrveranstaltungen /media/hpi/lehrveranstaltungen -o credentials=/home/moritz/.local/etc/hpi_sambacreds,workgroup=hpi,uid=moritz,gid=moritz
