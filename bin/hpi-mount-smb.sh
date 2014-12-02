7b9b3d5e4c1c2b0cf229d11d9d00416664a46e7e
m0r13 <moritz.hilscher@gmail.com>
2014-12-02 11:25:33 +0100

    Use networkmanager to determine if ssh tunnel is required.

--- bin/hpi-mount-smb.sh
+++ bin/hpi-mount-smb.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 hostname=$(hostname)
-wifiname=$(iwgetid 2>&1 | grep -Po "(?<=ESSID:\")[^\"]*")
+wifiname=$(nmcli dev | grep connected | grep wifi | awk '{ for(i=4;i<=NF;++i) print $i }')
+lanname=$(nmcli dev | grep connected | grep ethernet | awk '{ for(i=4;i<=NF;++i) print $i }')
 
-if [ "$hostname" = "moritz-desktop" ]; then
+if [ "$wifiname" = "hpi" -o "$lanname" = "hpi_lan" ]; then
+    mount -t cifs //FS23/moritz.hilscher$ /media/hpi/home -o credentials=/home/moritz/.local/etc/hpi_sambacreds,workgroup=hpi,uid=moritz,gid=moritz
+    mount -t cifs //FS23/lehrveranstaltungen /media/hpi/lehrveranstaltungen -o credentials=/home/moritz/.local/etc/hpi_sambacreds,workgroup=hpi,uid=moritz,gid=moritz
+else
     pkill "ssh*-L445:fs23:445"
     ssh -f -N moritz.hilscher@ssh-stud.hpi.uni-potsdam.de -L7445:fs23:445
     
     mount -t cifs //FS23/moritz.hilscher$ /media/hpi/home -o username=moritz.hilscher,workgroup=hpi,ip=127.0.0.1,port=7445,uid=moritz,gid=moritz
     mount -t cifs //FS23/lehrveranstaltungen /media/hpi/lehrveranstaltungen -o username=moritz.hilscher,workgroup=hpi,ip=127.0.0.1,port=7445,uid=moritz,gid=moritz
-elif [ "$hostname" = "moritz-laptop" ]; then
-    mount -t cifs //FS23/moritz.hilscher$ /media/hpi/home -o credentials=/home/moritz/.local/etc/hpi_sambacreds,workgroup=hpi,uid=moritz,gid=moritz
-    mount -t cifs //FS23/lehrveranstaltungen /media/hpi/lehrveranstaltungen -o credentials=/home/moritz/.local/etc/hpi_sambacreds,workgroup=hpi,uid=moritz,gid=moritz
 fi
 
 #mount -t cifs //FS23/moritz.hilscher$ /media/hpi/home -o credentials=/home/moritz/.local/etc/hpi_sambacreds,workgroup=hpi,uid=moritz,gid=moritz
