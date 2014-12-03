c04b6a630414c43108796f92793820f567dba441
m0r13 <moritz.hilscher@gmail.com>
2014-12-04 00:08:30 +0100

    Fehler bei nicht erkanntem Netzwerk.

--- bin/hpi-mount-smb.sh
+++ bin/hpi-mount-smb.sh
@@ -12,11 +12,13 @@ done
 if [ "$wifiname" = "hpi" -o "$lanname" = "hpi_lan" ]; then
     mount -t cifs //FS23/moritz.hilscher$ /media/hpi/home -o credentials=/home/moritz/.local/etc/hpi_sambacreds,workgroup=hpi,uid=moritz,gid=moritz
     mount -t cifs //FS23/lehrveranstaltungen /media/hpi/lehrveranstaltungen -o credentials=/home/moritz/.local/etc/hpi_sambacreds,workgroup=hpi,uid=moritz,gid=moritz
-else
+elif [ "$wifiname" != "" -o "$lanname" != "" ]; then
     pkill "ssh*-L445:fs23:445"
     ssh -f -N moritz.hilscher@ssh-stud.hpi.uni-potsdam.de -L7445:fs23:445
     
     mount -t cifs //FS23/moritz.hilscher$ /media/hpi/home -o username=moritz.hilscher,workgroup=hpi,ip=127.0.0.1,port=7445,uid=moritz,gid=moritz
     mount -t cifs //FS23/lehrveranstaltungen /media/hpi/lehrveranstaltungen -o username=moritz.hilscher,workgroup=hpi,ip=127.0.0.1,port=7445,uid=moritz,gid=moritz
+else
+    echo "Unable to detect network!"
 fi
 
