0419cf98f782a8426d63507616d1c21b8d2fa98d
Moritz Hilscher <moritz.hilscher@gmail.com>
2015-04-13 16:08:33 +0200

    Adjusted hpi sync script for math II lecture.

--- bin/hpi-sync-lehrveranstaltungen.sh
+++ bin/hpi-sync-lehrveranstaltungen.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-rsync -av --progress /media/hpi/lehrveranstaltungen/Wirtschaftliche\ Grundlagen/Wirtschaftliche\ Grundlagen\ WS\ 2014-15/ ~/Documents/hpi/1_ws1415/wirtschaft/
-rsync -av --progress /media/hpi/lehrveranstaltungen/Grundlagen_digitaler_Systeme/ ~/Documents/hpi/1_ws1415/gds/
-rsync -av --progress /media/hpi/lehrveranstaltungen/Modellierung\ I/ ~/Documents/hpi/1_ws1415/mod1/
+#rsync -av --progress "/media/hpi/lehrveranstaltungen/Wirtschaftliche Grundlagen/Wirtschaftliche Grundlagen WS 2014-15/" ~/hpi/1_ws1415/wirtschaft/
+#rsync -av --progress "/media/hpi/lehrveranstaltungen/Grundlagen_digitaler_Systeme/" ~/hpi/1_ws1415/gds/
+#rsync -av --progress "/media/hpi/lehrveranstaltungen/Modellierung I/" ~/hpi/1_ws1415/mod1/
+
+rsync -av --progress "/media/hpi/lehrveranstaltungen/Mathematik_II_SS2015/" ~/hpi/2_ss15/mathe2/
 
