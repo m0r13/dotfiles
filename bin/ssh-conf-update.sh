cf9079a77ca9bba5a1ef7d4b3a7d4a7f56733f5d
m0r13 <moritz.hilscher@gmail.com>
2015-09-26 18:52:04 -0400

    Small typo...

--- bin/ssh-conf-update.sh
+++ bin/ssh-conf-update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ssh-conf-generate.py ~/bin/hetzner-hosts.json > ~/.ssh/42-hetzner.sshconf
-ssh-conf-generate.py ~/bin/hetzner-hosts.json > ~/.ssh/50-bpt.sshconf
+ssh-conf-generate.py ~/bin/bpt-hosts.json > ~/.ssh/50-bpt.sshconf
 ssh-conf-merge.sh
 
