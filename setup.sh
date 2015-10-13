8d054e1d428da3c942f5ea24b93c267d16ac26ff
Moritz Hilscher <moritz.hilscher@gmail.com>
2015-10-13 17:59:36 +0200

    Improved dotfiles setup script a lot.

--- setup.sh
+++ setup.sh
@@ -1,17 +1,51 @@
 #!/bin/bash
 
-ln -s dotfiles/bin ../bin
+function link_file {
+    file_type=$1
+    src=$2
+    dest="$HOME/$3"
+    
+    if [ -h "$dest" ]; then
+        echo "$dest is already a symlink."
+        return
+    fi
 
-ln -s dotfiles/bashrc ../.bashrc
-ln -s dotfiles/bash_profile ../.bash_profile
-ln -s dotfiles/bash_exports ../.bash_exports
-ln -s dotfiles/bash_aliases ../.bash_aliases
-ln -s dotfiles/bash_prompt ../.bash_prompt
+    if [[ "$file_type" = "file" && -f "$dest" ]]; then
+        echo -n "$dest is an existing file. Should I replace it? <y|n>"
+        read answer
+        if [[ "$answer" = "" || "$answer" = "n" ]]; then
+            echo "$dest was skipped."
+            return
+        fi
+        rm "$dest"
+    elif [[ "$file_type" = "dir" && -d "$dest" ]]; then
+        echo -n "$dest is an existing directory. Should I replace it? <y|n>"
+        read answer
+        if [[ "$answer" = "" || "$answer" = "n" ]]; then
+            echo "$dest was skipped."
+            return
+        fi
+        rm -rf "$dest"
+    fi
 
-ln -s dotfiles/tmux.conf ../.tmux.conf
-ln -s dotfiles/vimrc ../.vimrc
-ln -s dotfiles/zshrc ../.zshrc
+    echo "$dest was created."
+    ln -s "$src" "$dest"
+}
 
-ln -s dotfiles/ycm_extra_conf.py ../.ycm_extra_conf.py
+for file in bashrc \
+            bash_profile \
+            bash_exports \
+            bash_aliases \
+            bash_prompt \
+            tmux.conf \
+            vimrc \
+            zshrc \
+            ycm_extra.conf.py \
+            xinitrc; do
+    link_file "file" "dotfiles/$file" ".$file"
+done
 
-ln -s dotfiles/i3 ../.i3
+link_file "dir" i3 .i3
+link_file "dir" ../dotfiles/config/dunst .config/dunst
+link_file "dir" ../dotfiles/config/pulseaudio-ctl .config/pulseaudio-ctl
+
