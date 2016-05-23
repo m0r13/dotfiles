e1d0ec984080f6c860bd9a0a27db5b08f5ad6d39
m0r13 <moritz.hilscher@gmail.com>
2016-05-23 09:52:35 +0200

    Improved battery script.

--- bin/i3-battery.sh
+++ bin/i3-battery.sh
@@ -2,7 +2,12 @@
  
 while true
 do
-#    paste /sys/class/power_supply/BAT0/uevent /sys/class/power_supply/BAT1/uevent | awk '{split($0,a,"="); split(a[2],b," "); (a[3] == "Charging" || b[1] == "Charging") ? $5 = "Charging" : (a[1] == "POWER_SUPPLY_POWER_NOW" ? $5 = a[3] + b[1] : $5 = (a[3] + b[1])/2); print a[1] "=" $5}' > ~/.uevent
-    paste /sys/class/power_supply/BAT0/uevent /sys/class/power_supply/BAT1/uevent | /home/moritz/bin/i3-battery.py > ~/.i3/battery
+    if [ -f /sys/class/power_supply/BAT1/uevent ]; then
+        paste /sys/class/power_supply/BAT0/uevent /sys/class/power_supply/BAT1/uevent | /home/moritz/bin/i3-battery.py > ~/.i3/battery
+        #paste /sys/class/power_supply/BAT0/uevent /sys/class/power_supply/BAT1/uevent | /home/moritz/bin/i3-battery.py
+    else
+        cp /sys/class/power_supply/BAT0/uevent ~/.i3/battery
+        #cat /sys/class/power_supply/BAT0/uevent
+    fi
     sleep 5
 done
